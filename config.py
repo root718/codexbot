65691278635e8196e121da2978eb2b12b9e34993
root718 <154349675+root718@users.noreply.github.com>
2023-12-22 22:35:44 +0530

    Update config.py
    added database

--- config.py
+++ config.py
@@ -28,8 +28,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6658497064"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "https://t.me/+uG5zal3YwPU0MzBl")
-DB_NAME = os.environ.get("DATABASE_NAME", "database")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://roottest720:39aJ7Hl7DEKarqHV@cluster0.jib1gi3.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "codexbot")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
