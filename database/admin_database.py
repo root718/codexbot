626ed72220a6946f99fa034481d546c73b57fd9b
root718 <154349675+root718@users.noreply.github.com>
2024-01-06 15:50:08 +0530

    Create admin_database.py

---
+++ database/admin_database.py
@@ -0,0 +1,31 @@
+import pymongo, os
+from config import DB_URI, DB_NAME
+
+
+dbclient = pymongo.MongoClient(DB_URI)
+database = dbclient[DB_NAME]
+
+admin_data = database['admins']
+
+
+async def full_adminbase():
+    admin_docs = admin_data.find()
+    admin_ids = []
+    for doc in admin_docs:
+        admin_ids.append(doc['_id'])
+    return admin_ids
+
+async def add_admin(admin_id: int):
+    admin_data.insert_one({'_id': admin_id})
+    return
+
+async def present_admin(admin_id : int):
+    found = admin_data.find_one({'_id': admin_id})
+    if found:
+        return True
+    else:
+        return False
+
+async def del_admin(admin_id: int):
+    admin_data.delete_one({'_id': admin_id})
+    return
