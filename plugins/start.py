2439710869f0956dee19edf15f230793acf0da11
root718 <154349675+root718@users.noreply.github.com>
2024-01-06 14:28:18 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -158,6 +158,12 @@ async def get_users(client: Bot, message: Message):
     users = await full_userbase()
     await msg.edit(f"{len(users)} users are using this bot")
 
+@Bot.on_message(filters.command('admins') & filters.private & filters.user(ADMINS))
+async def get_users(client: Bot, message: Message):
+    msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
+    # users = await full_userbase()
+    await msg.edit(f"Land")
+
 @Bot.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS))
 async def send_text(client: Bot, message: Message):
     if message.reply_to_message:
